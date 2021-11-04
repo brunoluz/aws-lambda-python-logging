71ae308bd6362d5735ba9e5c910ff68a4b0fc955
Bruno Luz <bruno.al.luz@gmail.com>
2021-11-03 21:05:36 -0300

    time added to logs

--- logs.py
+++ logs.py
@@ -1,22 +1,28 @@
 import logging
 from pythonjsonlogger import jsonlogger
+from datetime import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = jsonlogger.JsonFormatter()
 logger.handlers[0].setFormatter(formatter)
 
+TIME_FORMAT = '%Y/%m/%d %H:%M:%S.%f'
+
 
 def info(context, msg):
     context.order += 1
-    logger.info(msg, extra={'level': 'INFO', 'correlationId': context.correlationId, 'order': context.order})
+    logger.info(msg, extra={'level': 'INFO', 'correlationId': context.correlationId, 'order': context.order,
+                            'datetime': datetime.now().strftime(TIME_FORMAT)})
 
 
 def warning(context, msg):
     context.order += 1
-    logger.warning(msg, extra={'level': 'WARN', 'correlationId': context.correlationId, 'order': context.order})
+    logger.warning(msg, extra={'level': 'WARN', 'correlationId': context.correlationId, 'order': context.order,
+                               'datetime': datetime.now().strftime(TIME_FORMAT)})
 
 
 def error(context, msg):
     context.order += 1
-    logger.error(msg, extra={'level': 'ERROR', 'correlationId': context.correlationId, 'order': context.order})
+    logger.error(msg, extra={'level': 'ERROR', 'correlationId': context.correlationId, 'order': context.order,
+                             'datetime': datetime.now().strftime(TIME_FORMAT)})
